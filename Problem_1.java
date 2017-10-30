037ef002c4801efbf882422cdc8c8796b17b851b
Haim Tagamlitsky <haimt26@hotmail.com>
2017-10-30 23:25:12 +0000

    Update Problem_1.java

--- Problem_1.java
+++ Problem_1.java
@@ -7,7 +7,7 @@ public class Problem_1 // Multiples of 3 and 5
 	public static void main(String args[])
 	{	
 		int sum = 0;
-		for(int i = 0; i<= 1000; i++)
+		for(int i = 0; i< 1000; i++)
 		{
 			if(i%3 == 0 | i%5 == 0)
 			{
